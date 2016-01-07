cc0f4d9aa585283bff3a082e73cc41e85b3ee52c
Alexis Tual <alexist@jfrog.com>
2016-01-07 23:15:21 +0100

    Fixing img src issue

--- app/app.js
+++ app/app.js
@@ -2,37 +2,32 @@
     'use strict';
 
     angular.module('frogs', ['ngAnimate', 'ui.bootstrap'])
-        .constant('wsUrl', 'http://localhost:9000/frogs')
+        .constant('wsBaseUrl', 'http://172.16.61.131:9000/')
         .factory('frogsService', frogsService)
         .controller('FrogsController', FrogsController);
 
-    FrogsController.$inject = ['frogsService'];
-    function FrogsController(frogsService) {
+    FrogsController.$inject = ['frogsService', 'wsBaseUrl'];
+    function FrogsController(frogsService, wsBaseUrl) {
         var vm = this;
+        vm.wsBaseUrl = wsBaseUrl;
         frogsService.getFrogs().then(function (result) {
             vm.frogs = result.data
         });
     }
 
-    frogsService.$inject = ['$http', 'wsUrl'];
-    function frogsService($http, wsUrl) {
-
+    frogsService.$inject = ['$http', 'wsBaseUrl'];
+    function frogsService($http, wsBaseUrl) {
         return {
-
             getFrogs: function () {
-
                 var req = {
                     method: 'GET',
-                    url: wsUrl,
+                    url: wsBaseUrl + 'frogs',
                     headers: {
                         'Content-Type': 'application/json'
                     }
                 };
-
                 return $http(req);
             }
-
-
         }
     }
 
